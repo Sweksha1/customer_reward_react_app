9a86db097ae2b9e2166741feb920126ffce282c5
Sweksha <38045170+Sweksha1@users.noreply.github.com>
2024-08-05 17:23:57 +0530

    added total points

--- customer_reward_app_fe/src/components/Transaction.js
+++ customer_reward_app_fe/src/components/Transaction.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import '../styles/App.css';
 
-const Transactions = ({rewards, loading, error}) => {
+const Transactions = ({ rewards, loading, error }) => {
+
+  // Helper function to calculate total points for a customer
+  const calculateTotalPoints = (monthlyPoints) => {
+    return Object.values(monthlyPoints).reduce((total, points) => total + points, 0);
+  };
 
   return (
     <div className='transactions'>
@@ -12,19 +17,28 @@ const Transactions = ({rewards, loading, error}) => {
         <>
           {error && <p className='error'>{error}</p>}
           <div className='cards-container'>
-            {Object.keys(rewards).map(customerName => (
-              <div className='card' key={customerName}>
-                <h2>{customerName}</h2>
-                <div className='months'>
-                  {Object.keys(rewards[customerName].monthly).map(month => (
-                    <div className='month-item' key={month}>
-                      <p>Month {month}:</p>
-                        <span className='monthlyPoints'>{rewards[customerName].monthly[month]} points</span>
-                    </div>
-                  ))}
+            {Object.keys(rewards).map(customerName => {
+              const monthlyPoints = rewards[customerName].monthly;
+              const totalPoints = calculateTotalPoints(monthlyPoints);
+
+              return (
+                <div className='card' key={customerName}>
+                  <h2>{customerName}</h2>
+                  <div className='months'>
+                    {Object.keys(monthlyPoints).map(month => (
+                      <div className='month-item' key={month}>
+                        <p>Month {month}:</p>
+                        <span className='monthlyPoints'>{monthlyPoints[month]} points</span>
+                      </div>
+                    ))}
+                  </div>
+                  <div className='month-item'>
+                  <p>Total Points:</p>
+                    <span className='totalPoints'>{totalPoints} points</span>
+                  </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </>
       )}
